282071b378983ca0170a98e35d090ff7c69537c3
Edmundo <jmundo94@gmail.com>
2016-03-14 22:09:46 -0600

    Se terminaron los rounds, faltan 32-bit swap y Final Permutation

--- DES/des.rb
+++ DES/des.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require_relative "s-box"
 # TODO If the plainText is greater than 64 bits it has to run this two times, but running each 64-bit
 # blocks again... If there is a shorter block (less than 64-bit), then it has to add 0's to the
 # beggining of the block
@@ -6,6 +7,7 @@ class Des
 
 def initialize(plainText="01234567")
    plainBinText = getBin(plainText)
+   creatingSBoxes
    encipherment(plainBinText)
 end
 
@@ -20,7 +22,17 @@ def encipherment(plainBinText)
    twoHalvesArr = divideInTwo(iPResult)
    leftArr = twoHalvesArr[0]
    rightArr = twoHalvesArr[1]
-   round(rightArr)
+   i = 0
+   while i < 16
+      roundResult = round(rightArr)
+      tempLeft = leftArr
+      leftArr = rightArr
+      rightArr = xORRound(roundResult, tempLeft)
+      i += 1
+   end
+
+   # TODO hace falta 32-bit Swap
+   binding.pry
 
    return true
 
@@ -45,7 +57,7 @@ def getBin(plainText)
    end
    return matriz
 end
-# This methos does the initial permutation
+# This method does the initial permutation
 def initialPermutation(plainBinText)
    matrizIP = Array.new()
    # Top Half done with the following block of code
@@ -93,35 +105,143 @@ def divideInTwo(iPResult)
    wholeArr.push(leftArr)
    return wholeArr
 end
-# This method has TODO the whole round
+# This method calls every method of a round
 def round (rightArr)
    expandedArray = expansion(rightArr)
-
-
+   xORArray = xORRound(expandedArray, expandedArray) # Se necesita cambiar el segundo parámetro por la llave
+   substitutionArray = substitution(xORArray)
+   permutedArray = permutation(substitutionArray)
+   return permutedArray
 end
 # Expansion Permutation method
 def expansion(rightArr)
    expandedArray = Array.new()
    i = 0 # Column
-   # TODO
-   # Three cases:
-   # If the first position of the set is 0, it has to add the last number of the array to the beggining
-   if i == 0
-      
-
-   # If the last position of the set is the (length-1), it has to add the first number of the array to the last position (push)
-   elsif i == rightArr.length-1
-
-
-   # Else, it has to add the number of the previous position at the beggining and the number of the following position to the end
-   else
-
+   j = 5
+   expandedArray.push(rightArr[rightArr.length-1])
+   while j < 34
+      while i < j
+         expandedArray.push(rightArr[i])
+         i += 1
+      end
+      i -= 2
+      j += 4
    end
-
-
-
+   # It use the first bit and put it at the last position of the array
+   expandedArray.push(rightArr[0])
+   expandedArray.delete_at(-2)
    return expandedArray
+end
+# This method realizes the XOR function over the expandedArray and the key; both with 48 bits
+def xORRound (expandedArray, keyBitArr)
+   xORArray = Array.new(expandedArray.length)
+   # First it has to change the array of String values to Integers
+   expandedArray = convertStrArrToIntArr(expandedArray)
+   keyBitArr = convertStrArrToIntArr(keyBitArr)
+   i = 0
+   while i < expandedArray.length
+      xORArray[i] = expandedArray[i]^keyBitArr[i]
+      i += 1
+   end
+   return xORArray
+end
 
+def substitution(xORArray)
+   bitsBlock = 1 # From 1 to 8 (bitsBlock < 9)
+   xORArray = convertIntArrToStrArr(xORArray)
+   substitutionArray = ""
+   i = 0
+   j = 5
+   while bitsBlock < 9
+      sixArray = xORArray[i..j]
+      outerBits = sixArray[0]+sixArray[sixArray.length-1]
+      innerBits = ""
+      a=1
+      while a < 5
+         innerBits = innerBits + sixArray[a]
+         a+=1
+      end
+      outerBits = outerBits.to_i(2)
+      innerBits = innerBits.to_i(2)
+      # outerBits = "10".to_i(2) Pruebas
+      # innerBits = "0111".to_i(2) Pruebas
+      case bitsBlock
+      when 1
+         selectedCell = @sB1[outerBits][innerBits]
+      when 2
+         selectedCell = @sB2[outerBits][innerBits]
+      when 3
+         selectedCell = @sB3[outerBits][innerBits]
+      when 4
+         selectedCell = @sB4[outerBits][innerBits]
+      when 5
+         selectedCell = @sB5[outerBits][innerBits]
+      when 6
+         selectedCell = @sB6[outerBits][innerBits]
+      when 7
+         selectedCell = @sB7[outerBits][innerBits]
+      when 8
+         selectedCell = @sB8[outerBits][innerBits]
+      end
+      i += 6
+      j += 6
+      bitsBlock += 1
+      substitutionArray = substitutionArray + selectedCell
+   end
+   # This method returns a string, but to keep with the array model used, it has to pass
+   # each char to a new array. The following metho does the trick. (fromStrToArray)
+   substitutionArray = fromStrToArray(substitutionArray)
+   return substitutionArray
+end
+def permutation(substitutionArray)
+   orderArray = [16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25]
+   permutedArray = Array.new()
+   i = 0
+   while i < orderArray.length
+      j = orderArray[i]-1
+      permutedArray.push(substitutionArray[j])
+      i += 1
+   end
+   return permutedArray
+end
+
+def fromStrToArray(string)
+   newArray = Array.new()
+   string.each_char { |chr| newArray.push(chr) }
+   return newArray
+end
+def creatingSBoxes
+   sBox = SBox.new()
+   @sB1 = sBox.getSB1
+   @sB2 = sBox.getSB2
+   @sB3 = sBox.getSB3
+   @sB4 = sBox.getSB4
+   @sB5 = sBox.getSB5
+   @sB6 = sBox.getSB6
+   @sB7 = sBox.getSB7
+   @sB8 = sBox.getSB8
+end
+
+# This method convert an array of string to an array of integers
+def convertStrArrToIntArr (arrayToConvert)
+   arrayConverted = Array.new(arrayToConvert.length)
+   i = 0
+   while i < arrayToConvert.length
+      arrayConverted[i] = arrayToConvert[i].to_i
+      i += 1
+   end
+   return arrayConverted
+end
+
+# This method convert an array of string to an array of integers
+def convertIntArrToStrArr (arrayToConvert)
+   arrayConverted = Array.new(arrayToConvert.length)
+   i = 0
+   while i < arrayToConvert.length
+      arrayConverted[i] = arrayToConvert[i].to_s
+      i += 1
+   end
+   return arrayConverted
 end
 
 end
